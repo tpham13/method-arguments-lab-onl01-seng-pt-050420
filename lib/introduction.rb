654eb4a485eb7f32254498c1e7e1c1967e948a3c
Thanh Pham <tpham0123@gmail.com>
2020-05-07 03:55:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
 def introduction(name, language)
-  puts "Hi, my name is #{name}. I speak #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end  
-introduction(Dan)
+introduction(Josh, Ruby)
